f7ff31f2abb2a1f107a4d88e739ce30a64509688
divyanshraheja69 <111868701+divyanshraheja69@users.noreply.github.com>
2024-01-04 00:13:47 +0530

    post api

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+dotenv.config({ path: './config.env' });
+require('./conn/db')
+const authRoutes = require('./routes/authRoutes').default
+
+
+
+const app = express();
+
+app.use("./api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome To Ecommerce App</h1>");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`.bgCyan.white);
+});
+
